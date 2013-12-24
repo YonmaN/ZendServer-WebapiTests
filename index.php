0a5a36fe9ed80fb756fc6f2f8793a8eaddfbbfdd
yonni <yonni.m@zend.com>
2013-12-24 10:34:07 +0200

    Generalized index

--- index.php
+++ index.php
@@ -3,68 +3,9 @@
 </head>
 <body>
 <ul>
-<li><a href="WebApi-ServerReconfigure.php" target="_blank">/WebApi-ServerReconfigure.php</a></li>
-<li><a href="WebApi-VhostEdit.php" target="_blank">/WebApi-VhostEdit.php</a></li>
-<li><a href="WebApi-JobQueueAdd.php" target="_blank">/WebApi-JobQueueAdd.php</a></li>
-<li><a href="WebApi-DataCacheClear.php" target="_blank">/WebApi-DataCacheClear.php</a></li>
-<li><a href="WebApi-ApplicationRemove.php" target="_blank">/WebApi-ApplicationRemove.php</a></li>
-<li><a href="WebApi-SetPassword.php" target="_blank">/WebApi-SetPassword.php</a></li>
-<li><a href="WebApi-VhostValidateTemplate.php" target="_blank">/WebApi-VhostValidateTemplate.php</a></li>
-<li><a href="WebApi-GetNotifications.php" target="_blank">/WebApi-GetNotifications.php</a></li>
-<li><a href="WebApi-auditSettings.php" target="_blank">/WebApi-auditSettings.php</a></li>
-<li><a href="WebApi-JobRequeue.php" target="_blank">/WebApi-JobRequeue.php</a></li>
-<li><a href="WebApi-LibraryDeploy.php" target="_blank">/WebApi-LibraryDeploy.php</a></li>
-<li><a href="WebApi-LibraryVersionGetStatus.php" target="_blank">/WebApi-LibraryVersionGetStatus.php</a></li>
-<li><a href="WebApi-DaemonsProbe.php" target="_blank">/WebApi-DaemonsProbe.php</a></li>
-<li><a href="WebApi-LicenseUpdated.php" target="_blank">/WebApi-LicenseUpdated.php</a></li>
-<li><a href="WebApi-ConfigurationExport.php" target="_blank">/WebApi-ConfigurationExport.php</a></li>
-<li><a href="WebApi-PageCacheRulesList.php" target="_blank">/WebApi-PageCacheRulesList.php</a></li>
-<li><a href="WebApi-JobRuleInfo.php" target="_blank">/WebApi-JobRuleInfo.php</a></li>
-<li><a href="WebApi-JobInfo.php" target="_blank">/WebApi-JobInfo.php</a></li>
-<li><a href="WebApi-AddApiKey.php" target="_blank">/WebApi-AddApiKey.php</a></li>
-<li><a href="WebApi-DeleteNotification.php" target="_blank">/WebApi-DeleteNotification.php</a></li>
-<li><a href="WebApi-ExtensionsList.php" target="_blank">/WebApi-ExtensionsList.php</a></li>
-<li><a href="WebApi-ApplicationServersStatus.php" target="_blank">/WebApi-ApplicationServersStatus.php</a></li>
-<li><a href="WebApi-TokenGenerate.php" target="_blank">/WebApi-TokenGenerate.php</a></li>
-<li><a href="WebApi-ServerInfo.php" target="_blank">/WebApi-ServerInfo.php</a></li>
-<li><a href="WebApi-ExportIssuebyEventsGroup.php" target="_blank">/WebApi-ExportIssuebyEventsGroup.php</a></li>
-<li><a href="WebApi-Restart.php" target="_blank">/WebApi-Restart.php</a></li>
-<li><a href="WebApi-ServersList.php" target="_blank">/WebApi-ServersList.php</a></li>
-<li><a href="WebApi-MonitorGetIssuesByPredefinedFilters.php" target="_blank">/WebApi-MonitorGetIssuesByPredefinedFilters.php</a></li>
-<li><a href="WebApi-IssuesList.php" target="_blank">/WebApi-IssuesList.php</a></li>
-<li><a href="WebApi-MonitorSetRulesUpdated.php" target="_blank">/WebApi-MonitorSetRulesUpdated.php</a></li>
-<li><a href="WebApi-CodetracingTracingGetInfo.php" target="_blank">/WebApi-CodetracingTracingGetInfo.php</a></li>
-<li><a href="WebApi-CodetracingIsEnabled.php" target="_blank">/WebApi-CodetracingIsEnabled.php</a></li>
-<li><a href="WebApi-ClusterGetServerStatus.php" target="_blank">/WebApi-ClusterGetServerStatus.php</a></li>
-<li><a href="WebApi-TasksComplete.php" target="_blank">/WebApi-TasksComplete.php</a></li>
-<li><a href="WebApi-GetSystemInfo.php" target="_blank">/WebApi-GetSystemInfo.php</a></li>
-<li><a href="WebApi-StoreLicense.php" target="_blank">/WebApi-StoreLicense.php</a></li>
-<li><a href="WebApi-JobqueueStatistics.php" target="_blank">/WebApi-JobqueueStatistics.php</a></li>
-<li><a href="WebApi-ClusterGetServersCount.php" target="_blank">/WebApi-ClusterGetServersCount.php</a></li>
-<li><a href="WebApi-BootstrapSingleServer.php" target="_blank">/WebApi-BootstrapSingleServer.php</a></li>
-<li><a href="WebApi-SendEmail.php" target="_blank">/WebApi-SendEmail.php</a></li>
-<li><a href="WebApi-AuthenticationSettings.php" target="_blank">/WebApi-AuthenticationSettings.php</a></li>
-<li><a href="WebApi-BacktraceFile.php" target="_blank">/WebApi-BacktraceFile.php</a></li>
-<li><a href="WebApi-CodetracingTracefileDownload-byEventGroupId.php" target="_blank">/WebApi-CodetracingTracefileDownload-byEventGroupId.php</a></li>
-<li><a href="WebApi-CodetracingTracingList.php" target="_blank">/WebApi-CodetracingTracingList.php</a></li>
-<li><a href="WebApi-CodetracingDelete.php" target="_blank">/WebApi-CodetracingDelete.php</a></li>
-<li><a href="WebApi-CodetracingTracefileDownload.php" target="_blank">/WebApi-CodetracingTracefileDownload.php</a></li>
-<li><a href="WebApi-ConfigurationDirectivesList.php" target="_blank">/WebApi-ConfigurationDirectivesList.php</a></li>
-<li><a href="WebApi-StatsGetSeries.php" target="_blank">/WebApi-StatsGetSeries.php</a></li>
-<li><a href="WebApi-MonitorExportRules.php" target="_blank">/WebApi-MonitorExportRules.php</a></li>
-<li><a href="WebApi-PagecacheExportRules.php" target="_blank">/WebApi-PagecacheExportRules.php</a></li>
-<li><a href="WebApi-ApplicationRedeploy.php" target="_blank">/WebApi-ApplicationRedeploy.php</a></li>
-<li><a href="WebApi-ApplicationGetDetails_id-1.php" target="_blank">/WebApi-ApplicationGetDetails.php with id=7</a></li>
-<li><a href="WebApi-ApplicationGetStatusFailOldVersion.php" target="_blank">/WebApi-ApplicationGetStatusFailOldVersion.php</a></li>
-<li><a href="WebApi-ApplicationGetStatusFail.php" target="_blank">/WebApi-ApplicationGetStatusFail.php</a></li>
-<li><a href="WebApi-ApplicationGetStatus_id-1.php" target="_blank">/WebApi-ApplicationGetStatus.php with id=1</a></li>
-<li><a href="WebApi-ApplicationGetStatus.php" target="_blank">/WebApi-ApplicationGetStatus.php</a></li>
-<li><a href="WebApi-ApplicationDeploy.php" target="_blank">/WebApi-ApplicationDeploy.php</a></li>
-<li><a href="WebApi-ApplicationUpdate.php" target="_blank">/WebApi-ApplicationUpdate.php</a></li>
-<li><a href="WebApi-RedeployAllApplications.php" target="_blank">/WebApi-RedeployAllApplications.php</a></li>
-<li><a href="WebApi-EventsGroupDetails.php" target="_blank">/WebApi-EventsGroupDetails.php</a></li>
-<li><a href="WebApi-IssueDetails.php" target="_blank">/WebApi-IssueDetails.php</a></li>
-<li><a href="WebApi-Fail.php" target="_blank">/WebApi-Fail.php</a></li>
+<?php foreach (glob('WebApi-*') as $script):?>
+<li><a href="<?php echo $script ?>" target="_blank">/<?php echo $script ?></a></li>
+<?php endforeach ?>
 </ul>
 </body>
 </html>
