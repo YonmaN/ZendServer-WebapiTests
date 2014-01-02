0790106176d09528e0a01330cfe5de877f23a87e
yonni <yonni.m@zend.com>
2014-01-02 09:25:54 +0200

    Library deploy to test user params

--- WebApi-LibraryDeploy.php
+++ WebApi-LibraryDeploy.php
@@ -1,35 +1,27 @@
 <?php
 require_once 'bootstrap.php';
 
-$userAgent = 'Zend Server Development';
-$date = gmdate('D, d M Y H:i:s') . ' GMT';
-$uri = Zend_Uri::factory('http://yonni-desktop:10081/ZendServer/Api/libraryVersionDeploy');
-/**
- * The following variable should hold the same preshared key of the server set in
- * zend-server-user.ini under zendServer\apiKey
- */
-$psk = $WebapiKey->HASH;
-$keyName = $WebapiKey->NAME;
-
-$signatureGenerator = new \WebAPI\SignatureGenerator();
-$signature =
-$signatureGenerator->setHost("{$uri->getHost()}:{$uri->getPort()}")
-				->setUserAgent($userAgent)
-				->setDate($date)
-				->setRequestUri($uri->getPath())
-				->generate($psk);
-                                       
-
-$client = new Zend_Http_Client();
+$client = new WebAPIClient('http://yonni-desktop:10081/ZendServer/Api/libraryVersionDeploy');
 $client
-	->setUri($uri)
-	->setHeaders(array(
-		'User-Agent'        => $userAgent,
-		'Date'              => $date,
-		'X-Zend-Signature'  => $keyName.';' . $signature,
-		'Accept'	=> 'application/vnd.zend.serverapi+xml;version=1.5'
-	))
-	->setFileUpload('/usr/zlocal/packaging/qa-stuff/apps/ZF2/zf2-2_0.zpk', 'fileupload');
+->setEncType(\Zend\Http\Client::ENC_FORMDATA)
+->setMethod('POST')
+->setFileUpload('/usr/zlocal/packaging/qa-stuff/apps/ibmi toolkit/PHPToolkitForIBMi-1.5.1-userparams.zpk', 'fileupload')
+->getRequest()->getPost()->fromArray(array('userParams' => array(
+		'db_type' => 'adsasd', 
+		'db_name' => 'asdasd', 
+		'db_username' => 'asdasd', 
+		'db_password' => 'asdfasd', 
+		'db_host' => 'asddasads',
+		'site_name' => 'asddasads',
+		'site_email' => 'local-part@hostname.com',
+		'admin_username' => 'asddasads',
+		'admin_email' => 'local-part@hostname.com',
+		'admin_password' => 'asddasads',
+		'admin_confirm_password' => 'asddasads',
+		'server_timezone' => 'asddasads',
+		'server_clean_urls' => 'asddasads',
+		'server_update_notifications' => 'asddasads',
+)));
 
 
 //	$client->setCookie('debug_host', '10.1.2.174');
@@ -38,8 +30,7 @@ $client
 //	$client->setCookie('debug_session_id', rand(3000, 1000000));
 //	$client->setCookie('ZDEDebuggerPresent', 'php%2Cphtml%2Cphp3');
 
-$client->setConfig(array('timeout' => 60));
-$response = $client->request(Zend_Http_Client::POST);
+$response = $client->send();
 echo cleaning(nl2br(htmlentities($response->getBody())));
 
 
