7fe0a2d657c885289c8636fc4b735b87a07bdd7c
yonni <yonni.m@zend.com>
2014-01-20 17:21:34 +0200

    Added configurerevert/apply tests

---
+++ WebApi-ConfigurationApplyChanges.php
@@ -0,0 +1,25 @@
+<?php
+require_once 'bootstrap.php';
+                                       
+$client = new WebAPIClient('http://yonni-desktop:10081/ZendServer/Api/configurationApplyChanges');
+$client
+	->setEncType(\Zend\Http\Client::ENC_FORMDATA)
+	->setMethod('POST')
+	->getRequest()->getPost()->fromArray(
+	    array('serverId' => 0)
+    );
+	
+	
+//	$client->setCookie('debug_host', '10.1.2.174');
+//	$client->setCookie('debug_port', '10137');
+//	$client->setCookie('_bm', '2897');
+//	$client->setCookie('debug_session_id', rand(3000, 1000000));
+//	$client->setCookie('ZDEDebuggerPresent', 'php%2Cphtml%2Cphp3');
+
+// $client->setConfig(array('timeout' => 60));
+//$client->setParameterGet('nodekey', '1234');
+
+$response = $client->send();
+echo '<pre>';
+echo htmlentities($response->getBody());
+
